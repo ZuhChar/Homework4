984a9765fcbd90c7323339d45913983b5e35dfd8
ZuhChar <zcrimmel@uwyo.edu>
2022-02-22 16:41:44 -0700

    g

--- wycat.c
+++ wycat.c
@@ -5,11 +5,21 @@
 *
 * COSC 3750, Homework 4
 *
-* This is a simple version of the cat utility. It is designed to
-* ...
+* This is a simple version of the cat utility. It is designed to print the contents of a file or read and write standard input if no arguments are given or if a '-' is given.
 *
 */
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main(){
+int main(int argc, char **argv){
+    char str[100];
+    if(sizeof argv[i] == 0){
+          gets(str);
+          puts(str);
+    }
+    // for(int i = 0: i < argc; i++){
+        
+    // }
     return 0;
 }
