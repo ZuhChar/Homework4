04bc6b688d57a25cb058db81d7a0e0939a499ea1
ZuhChar <zcrimmel@uwyo.edu>
2022-02-22 21:08:05 -0700

    g

--- wycat.c
+++ wycat.c
@@ -28,7 +28,6 @@ int main(int argc, char **argv){
         FILE *fp = fopen(argv[i], "rw");
         if( fp == NULL ){
             perror("No such file or directory\n");
-            fclose(fp);
         }else{
             fread(buffer, sizeof(buffer) + 1, 1, fp);
             fwrite(buffer, sizeof(buffer) + 1, 1, stdout);
